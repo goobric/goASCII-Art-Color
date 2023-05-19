c28597f81776bb97ba0f1d4004f26a5d1e4869d0
goobric <goobric@gmail.com>
2023-05-19 17:19:11 +0800

    added TrimAtoi function to convert string to integer

--- acc/main.go
+++ acc/main.go
@@ -19,3 +19,19 @@ func ReadLines(path string) ([]string, error) {
 	}
 	return lines, scanner.Err()
 }
+
+func TrimAtoi(s string) int {
+	str := []rune(s)
+	trim := 0
+
+	for i := 0; i < len(str); i++ {
+		// if !neg && trim == 0 && str[i] == '-' {
+		// 	neg = true
+		// }
+		if str[i] >= '0' && str[i] <= '9' {
+			trim *= 10
+			trim += int(str[i] - 48)
+		}
+	}
+	return trim
+}
